d0c8e47c4c30d6bc9cb21a687dc40ff6d54a5a25
Anton Potekhin <anton.potekhin@orbitscripts.com>
2017-12-21 12:24:43 +0300

    Add ability create square with center coords

--- library/src/main/java/com/anton111111/vr/widgets/Square.java
+++ library/src/main/java/com/anton111111/vr/widgets/Square.java
@@ -14,6 +14,10 @@ import java.nio.ShortBuffer;
 
 public class Square {
 
+    public static final int START_COORDS_TYPE_CENTER = 1;
+    public static final int START_COORDS_TYPE_LEFT_BOTTOM_CORNER = 2;
+
+
     private static final short[] VERTEX_INDEXES = new short[]{
             0, 1, 2, 0, 2, 3
     };
@@ -65,13 +69,33 @@ public class Square {
      * @param height
      */
     public Square(Context context, float[] startCoords, float width, float height) {
+        this(context, startCoords, START_COORDS_TYPE_LEFT_BOTTOM_CORNER, width, height);
+    }
+
+    /**
+     * Create square
+     *
+     * @param context
+     * @param startCoords     coords of left bottom corner of square
+     * @param startCoordsType type of startCoords (START_COORDS_TYPE_CENTER, START_COORDS_TYPE_LEFT_BOTTOM_CORNER)
+     * @param width
+     * @param height
+     */
+    public Square(Context context, float[] startCoords, int startCoordsType, float width, float height) {
         this.width = width;
         this.height = height;
+
+        float lbx = startCoords[0];
+        float lby = startCoords[1];
+        if (startCoordsType == START_COORDS_TYPE_CENTER) {
+            lbx -= width / 2.0f;
+            lby -= height / 2.0f;
+        }
         this.coords = new float[]{
-                startCoords[0], startCoords[1], startCoords[2],
-                startCoords[0] + width, startCoords[1], startCoords[2],
-                startCoords[0] + width, startCoords[1] + height, startCoords[2],
-                startCoords[0], startCoords[1] + height, startCoords[2],
+                lbx, lby, startCoords[2],
+                lbx + width, lby, startCoords[2],
+                lbx + width, lby + height, startCoords[2],
+                lbx, lby + height, startCoords[2],
         };
         init(context);
     }
